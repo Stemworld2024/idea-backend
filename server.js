49148257129a9aa6a76fdcd50c01b6b82874d081
Ganesh <ganeshb4004@gmail.com>
2026-04-27 12:46:54 +0530

    fix cores

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
